f8d83ee2505953d6710075e7927bd1723d6cb6b7
mzembron <mateuszzembron@gmail.com>
2022-08-04 22:10:24 +0200

    poprawka

--- mrs_main/scripts/test_turtlebot.py
+++ mrs_main/scripts/test_turtlebot.py
@@ -6,7 +6,7 @@ from nav_msgs.srv import GetPlan, GetPlanRequest
 def main():
     rospy.init_node('mrs_master')
     rospy.sleep(2)
-    robot1 = Turtlebot("/robot1/odom", '/robot1/move_base/goal' )
+    # robot1 = Turtlebot("/robot1/odom", '/robot1/move_base/goal' )
 
     ## Requesing plan exmple 
     make_plan = rospy.ServiceProxy('/robot1/move_base_node/NavfnROS/make_plan', GetPlan)
@@ -14,12 +14,12 @@ def main():
 
     req.start.header.frame_id = 'map'
     req.start.pose.position.x = 1.34
-    req.start.pose.position.x = 0.37
+    req.start.pose.position.y = 0.37
     req.start.pose.orientation.w = 1
 
     req.goal.header.frame_id = 'map'
     req.goal.pose.position.x = -4
-    req.goal.pose.position.x = 1
+    req.goal.pose.position.y = 1
     req.goal.pose.orientation.w = 1
 
     req.tolerance = 0.1
