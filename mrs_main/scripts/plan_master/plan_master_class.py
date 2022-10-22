e113717d59f9d80aa079cb5f8c680924b2114369
mzembron <mateuszzembron@gmail.com>
2022-10-22 23:26:01 +0200

    publishing tasks on topic

--- mrs_main/scripts/plan_master/plan_master_class.py
+++ mrs_main/scripts/plan_master/plan_master_class.py
@@ -4,15 +4,15 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 
 
-from plan_master.task.task import Task 
-from 
-
-
+from plan_master.task.task import Task
+from mrs_msgs.msg import TaskDesc
+from data_parser.room_coordinates_parser import RoomCoordinatesParser
 
 
 class PlanMaster():
     def __init__(self):
         rospy.Subscriber("/move_base_simple/goal", PoseStamped, self._move_base_goal_callback)
+        rospy.Subscriber("plan_master/order_task", TaskDesc, self._order_task_callback)
         #subscription of task on general topic like /plan_master/ordered_tasks
         self.robots = []
 
@@ -21,10 +21,20 @@ class PlanMaster():
 
     def _move_base_goal_callback(self, data):
         task = Task('GT', data, priority= 8)
+        self._order_task(task)
+
+    def _order_task_callback(self, task_desc):
+        try:
+            room_coordinates = RoomCoordinatesParser().get_room_coordinates(task_desc.data)
+            task_data = self._prepare_task_data(room_coordinates)
+            task = Task('GT', task_data, priority = task_desc.priority)
+            self._order_task(task)
+        except(KeyError):
+            print("Room name does not exist in system!")
+
+    def _order_task(self, task):
         optimal_robot, position = self._select_optimal_robot(task)
-
         optimal_robot.add_task(task, position)
-
 
     def _select_optimal_robot(self, task):
         robots_cost_dict = {}
@@ -39,5 +49,16 @@ class PlanMaster():
         optimal_robot = min(robots_cost_dict, key = robots_cost_dict.get) 
         return optimal_robot, robots_task_position_dict[optimal_robot]
         # return
-    
+
+    def _prepare_task_data(self, coordinates):
+        task_data = PoseStamped()
+        task_data.pose.position.x = coordinates[0]
+        task_data.pose.position.y = coordinates[1]
+        task_data.pose.position.z = 0
         
+        task_data.pose.orientation.x = 0
+        task_data.pose.orientation.y = 0 
+        task_data.pose.orientation.z = 0 
+        task_data.pose.orientation.w = 1 
+        return task_data
+
