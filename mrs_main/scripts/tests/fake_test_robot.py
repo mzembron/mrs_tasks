7c30403430206d500b889ede9ecc586513ac754f
mzembron <mateuszzembron@gmail.com>
2022-11-23 00:11:25 +0100

    flake8 refactor of robot_task_harmonizer

--- mrs_main/scripts/tests/fake_test_robot.py
+++ mrs_main/scripts/tests/fake_test_robot.py
@@ -1,5 +1,6 @@
 from constants.testing_constats import FAKE_BIG_LENGTH, FAKE_SMALL_LENGTH
 
+
 class FakeRobot:
 
     def __init__(self):
@@ -7,6 +8,6 @@ class FakeRobot:
 
     def calc_cost_from_curr_position_to_spec_position(self, goal_odom_data):
         return FAKE_BIG_LENGTH
-    
+
     def calc_cost_from_spec_position_to_spec_position(self, start_odom_data, goal_odom_data):
         return FAKE_SMALL_LENGTH
