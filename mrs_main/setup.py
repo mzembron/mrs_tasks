fa341ac126ac135782b1fc373be2a33bc0def95f
mzembron <mateuszzembron@gmail.com>
2023-10-23 20:41:36 +0200

    CMake replaced by setup.py

---
+++ mrs_main/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'mrs_main'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mateusz',
+    maintainer_email='mateuszzembron@gmail.com',
+    description='Framework for task allocation and coordination in distributed system',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'robo_agent = mrs_main.mrs_main:main',
+        ],
+    },
+)
