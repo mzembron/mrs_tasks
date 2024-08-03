f46f100c63086ec7fdb8744c2ac4b74fd3cbbeef
mzembron <mateuszzembron@gmail.com>
2024-08-03 13:38:07 +0000

    chore(WiP): use ros humble image

--- .devcontainer/update_bashrc.sh
+++ .devcontainer/update_bashrc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# echo 'source /opt/ros/noetic/setup.bash' >> ~/.bashrc
+echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
 # echo 'source /opt/siu_ws/devel/setup.bash' >> ~/.bashrc
-# echo 'export ROS_HOSTNAME=localhost' >> ~/.bashrc
+echo 'export ROS_HOSTNAME=localhost' >> ~/.bashrc
