fa9ff74b55391fc467edb77d8b404a7aa1ef8ecf
mzembron <mateuszzembron@gmail.com>
2024-08-03 16:09:10 +0200

    chore(WiP): source to mrs_ws inside container

--- .devcontainer/update_bashrc.sh
+++ .devcontainer/update_bashrc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+echo 'source /opt/mrs_ws/install/setup.bash' >> ~/.bashrc
 # echo 'source /opt/siu_ws/devel/setup.bash' >> ~/.bashrc
 echo 'export ROS_HOSTNAME=localhost' >> ~/.bashrc
