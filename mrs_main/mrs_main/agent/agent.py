cf7443a9dcc53913e3545a0cec1e147b0960308a
mzembron <mateuszzembron@gmail.com>
2024-08-13 18:47:19 +0200

    chore(WiP): set agent interest level as test script attribute

--- mrs_main/mrs_main/agent/agent.py
+++ mrs_main/mrs_main/agent/agent.py
@@ -10,8 +10,8 @@ class Agent():
         agent_number = str(uuid.uuid1().int)
         self.agent_name = 'agent_' + agent_number
         self.__task_manager = TaskManager(agent_name=self.agent_name,
-                                          intrest_exec=0.6,
-                                          intrest_coord=0.4)
+                                          intrest_exec=intrest_exec,
+                                          intrest_coord=intrest_coord)
         self.__orders_manager = OrdersManager(agent_name=self.agent_name,
                                               task_manager=self.__task_manager.interface)
 
--- mrs_main/mrs_main/agent_entrypoint.py
+++ mrs_main/mrs_main/agent_entrypoint.py
@@ -1,11 +1,18 @@
 import rclpy
+import sys
 from agent.agent import Agent
 
 
 def main(args=None):
     rclpy.init()
-
-    agent = Agent()
+    intrest_exec = 0.2
+    intrest_coord = 0.2
+    if (len(sys.argv)>1):
+        intrest_exec = float(sys.argv[1])
+    if (len(sys.argv)>1):
+        intrest_coord = float(sys.argv[2])
+        
+    agent = Agent(intrest_exec=intrest_exec, intrest_coord=intrest_coord)
     agent.start_agent()
 
 
