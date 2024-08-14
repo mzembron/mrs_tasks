f3d0d1d32bd3da4db3b7ef1c3187c909b737b1a1
mzembron <mateuszzembron@gmail.com>
2024-08-14 21:16:29 +0200

    feat(WiP): add new performative types

--- mrs_main/mrs_main/common/conversation_data.py
+++ mrs_main/mrs_main/common/conversation_data.py
@@ -2,5 +2,9 @@ from enum import Enum
 
 
 class MrsConvPerform(str, Enum):
+    propose_exec_role = 'propose_exec_role'
+    accept_exec_proposal = 'acc_exec_proposal'
     declare_ex_intrest = 'dec_ex_intr'
     declare_coord_intrest = 'dec_coord_intr'
+    propose_coord_role = 'propose_coord_role'
+    accept_coord_proposal = 'acc_coord_proposal'
