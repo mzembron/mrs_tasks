9f5193e5ca9e85da503720be71c8d42ade13821a
mzembron <mateuszzembron@gmail.com>
2024-08-15 21:09:04 +0200

    refactor: change define_next from abstract to routing method

--- mrs_main/mrs_main/common/conversation_data.py
+++ mrs_main/mrs_main/common/conversation_data.py
@@ -6,5 +6,6 @@ class MrsConvPerform(str, Enum):
     accept_exec_proposal = 'acc_exec_proposal'
     declare_ex_intrest = 'dec_ex_intr'
     declare_coord_intrest = 'dec_coord_intr'
-    propose_coord_role = 'propose_coord_role'
+    # TODO: Decide if below should be removed
+    propose_coord_role = 'propose_coord_role' 
     accept_coord_proposal = 'acc_coord_proposal'
--- mrs_main/mrs_main/tasks_management/task_fsm.py
+++ mrs_main/mrs_main/tasks_management/task_fsm.py
@@ -20,48 +20,61 @@ class TaskFSM:
 
 class State(ABC):
     @property
-    def task(self) ->TaskFSM:
+    def task(self) -> TaskFSM:
         return self._task_fsm
 
     @task.setter
     def task(self, task_fsm: TaskFSM):
         self._task_fsm = task_fsm
 
-    @abstractmethod
     def define_next(self, msg: TaskConvMsg) -> TaskConvMsg:
-        """ Returns msg with the content specific to the current state """
-        pass
+        """ Routing method, runs method specific to communicate type,
+            thanks to that return adequate response or no response at all. """
+        if (msg.performative == MrsConvPerform.declare_coord_intrest): 
+            return self.respond_to_coord_intrest_declaration(msg)
+        elif (msg.performative == MrsConvPerform.declare_ex_intrest): 
+            return self.respond_to_exec_intrest_declaration(msg)
+        elif (msg.performative == MrsConvPerform.propose_exec_role):
+            return self.respond_to_exec_proposal(msg)
+        elif (msg.performative == MrsConvPerform.accept_exec_proposal):
+            return self.respond_to_exec_acceptance(msg)
+        
+    def respond_to_coord_intrest_declaration(self, msg: TaskConvMsg):
+        return
+    
+    def respond_to_exec_intrest_declaration(self, msg: TaskConvMsg):
+        return
+    
+    def respond_to_exec_proposal(self, msg: TaskConvMsg):
+        return
+    
+    def respond_to_exec_acceptance(self, msg: TaskConvMsg):
+        return
 
 class DefineTaskIntrest(State):
     INTREST_THRESHOLD = 0.5
-    def define_next(self, msg: TaskConvMsg) -> TaskConvMsg:
-        if (msg.performative == MrsConvPerform.declare_coord_intrest):
-            partner_intrest = float(msg.data[0])
-            reply_msg = TaskConvMsg()
-            if (partner_intrest > self.INTREST_THRESHOLD):
-                print(f'received partners intrest: {partner_intrest}')
-                reply_msg.performative = MrsConvPerform.propose_exec_role
-                reply_msg.data = [msg.sender]
-                print(f'Proposing coord role to: {msg.sender}')
-                self._task_fsm.transition_to(WaitForExec())
-            else:
-                reply_msg.performative = MrsConvPerform.declare_coord_intrest
-                temp_coord_intrest = '0.4' #TODO: remove coord intrest at all, 
-                                                # every agent should take part in supervising
-                reply_msg.data = [temp_coord_intrest]
-            reply_msg.short_id = msg.short_id
-            return reply_msg
+    def respond_to_coord_intrest_declaration(self, msg: TaskConvMsg) -> TaskConvMsg:
+        partner_intrest = float(msg.data[0])
+        reply_msg = TaskConvMsg() 
+        if (partner_intrest > self.INTREST_THRESHOLD):
+            print(f'received partners intrest: {partner_intrest}')
+            reply_msg.performative = MrsConvPerform.propose_exec_role
+            reply_msg.data = [msg.sender]
+            print(f'Proposing coord role to: {msg.sender}')
+            self._task_fsm.transition_to(WaitForExec())
         else:
-            print(f'Received msg with performative {msg.performative}')
+            reply_msg.performative = MrsConvPerform.declare_coord_intrest
+            temp_coord_intrest = '0.4' #TODO: remove coord intrest at all, 
+                                            # every agent should take part in supervising
+            reply_msg.data = [temp_coord_intrest]
+        reply_msg.short_id = msg.short_id
+        return reply_msg
 
 class WaitForExec(State):
-    def define_next(self, msg: TaskConvMsg):
-        print('Waiting for the right time...')
+    pass
 
 class ExecTask(State):
-    def define_next(self, msg: TaskConvMsg):
-        pass
+    pass
 
 class SuperviseTask(State):
-    def define_next(self, msg: TaskConvMsg):
-        pass
+    pass
