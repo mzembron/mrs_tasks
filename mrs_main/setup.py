939ad865a4fe7ef4477d573c3ad0a6f6af831611
mzembron <mateuszzembron@gmail.com>
2024-09-13 21:49:31 +0000

    chore: add networkx dependency for mrs_main package

--- mrs_main/setup.py
+++ mrs_main/setup.py
@@ -11,7 +11,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'networkx'
+        ],
     zip_safe=True,
     maintainer='Mateusz Zembron',
     maintainer_email='mateuszzembron@gmail.com',
