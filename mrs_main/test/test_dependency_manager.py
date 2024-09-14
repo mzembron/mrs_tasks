ebcb120b4bcf2f6676e8aa3fc396503dba57ee53
mzembron <mateuszzembron@gmail.com>
2024-09-14 07:24:20 +0000

    test(WiP): add unit tests for dependency_manager.py

---
+++ mrs_main/test/test_dependency_manager.py
@@ -0,0 +1,48 @@
+import pytest
+from mrs_main.tasks_management.dependency_manager import DependencyManager, TaskDependencyManager
+
+class TestDependencyManager:
+
+    @pytest.fixture
+    def setup(self):
+        tasks_dict = {}
+        self.dependency_manager = DependencyManager(tasks_dict)
+
+    def test_are_task_dependencies_met(self, setup):
+        self.dependency_manager.introduce_task_dependencies(1, [])
+        assert self.dependency_manager.are_task_dependencies_met(1) == True
+
+        self.dependency_manager.introduce_task_dependencies(2, [1])
+        assert self.dependency_manager.are_task_dependencies_met(2) == False
+
+    def test_update_dependencies(self, setup):
+        self.dependency_manager.introduce_task_dependencies(1, [])
+        self.dependency_manager.introduce_task_dependencies(2, [1])
+        
+        self.dependency_manager.update_dependencies(1)
+        assert self.dependency_manager.are_task_dependencies_met(2) == True
+
+    def test_introduce_task_dependencies(self, setup):
+        self.dependency_manager.introduce_task_dependencies(1, [2, 3])
+        assert self.dependency_manager._tasks_dependencies.has_node(1)
+        assert self.dependency_manager._tasks_dependencies.has_edge(2, 1)
+        assert self.dependency_manager._tasks_dependencies.has_edge(3, 1)
+
+class TestTaskDependencyManager:
+
+    @pytest.fixture
+    def setup(self):
+        tasks_dict = {}
+        self.dependency_manager = DependencyManager(tasks_dict)
+        self.task_dependency_manager = TaskDependencyManager(self.dependency_manager, 1, [2, 3])
+
+    def test_are_dependencies_met(self, setup):
+        assert self.task_dependency_manager.are_dependencies_met() == False
+
+        self.dependency_manager.update_dependencies(2)
+        self.dependency_manager.update_dependencies(3)
+        assert self.task_dependency_manager.are_dependencies_met() == True
+
+    def test_notify_on_finish(self, setup):
+        self.task_dependency_manager.notify_on_finish()
+        assert not self.dependency_manager._tasks_dependencies.has_node(1)
