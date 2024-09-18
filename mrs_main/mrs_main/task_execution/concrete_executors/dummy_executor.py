c06d588072682ce8129415d28838dfd76034bf3c
mzembron <mateuszzembron@gmail.com>
2024-09-18 21:18:34 +0000

    feat(WiP): add dummy executor

---
+++ mrs_main/mrs_main/task_execution/concrete_executors/dummy_executor.py
@@ -0,0 +1,5 @@
+class DummyExecutor:
+    """ class handles execution of tasks for specific type of agent,
+    in this case it is a dummy executor, which does nothing """
+    def __init__(self) -> None:
+        pass
