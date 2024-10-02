0adbe0c24406474b5fa264c5ef9174a384a31352
mzembron <mateuszzembron@gmail.com>
2024-10-02 19:33:07 +0000

    feat(WiP): add dummy implementation of starting the execution of task in separate thread

--- mrs_main/mrs_main/task_execution/concrete_executors/dummy_executor.py
+++ mrs_main/mrs_main/task_execution/concrete_executors/dummy_executor.py
@@ -1,17 +1,30 @@
+import threading
+import time
 from mrs_main.task_execution.concrete_executors.executor_interface import AbstractExecutor
 
 class DummyExecutor(AbstractExecutor):
     """ class handles execution of tasks for specific type of agent,
     in this case it is a dummy executor, which does nothing """
     def __init__(self) -> None:
-        pass
+        self.execution_thread = None
 
     def start_execution(self):
         """ entrypoint to trigger execution of task by external entity """
         #TODO: start mock of task execution in separate thread
-        pass
+        def mock_task_execution():
+            time.sleep(5)
+            print("[ DEBUG LOG ] Task execution completed.")
+            self._on_execution_finished()
+        
+        self.execution_thread = threading.Thread(target=mock_task_execution)
+        self.execution_thread.start()
+
 
     def get_execution_info(self):
         """ retrives info regarding task execution status """
         # retrieve info from separate thread mocking task execution
         pass
+
+    def _on_execution_finished(self):
+        """ callback method, called when task execution is finished """
+        print("[ DEBUG LOG ] Task execution finished.")
