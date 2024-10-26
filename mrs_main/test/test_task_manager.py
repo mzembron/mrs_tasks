ac9ddf91486732a99f36db75af8b751140e4ff4a
mzembron <mateuszzembron@gmail.com>
2024-10-26 19:49:46 +0000

    tests: adjust tests after removal of "concrete" task manager, remove useless tests, additional refactor

--- mrs_main/test/test_task_manager.py
+++ mrs_main/test/test_task_manager.py
@@ -6,38 +6,34 @@ from mrs_main.tasks_management.task_manager import TaskManager
 
 import json
 
-class TestTaskManagerInterface:
+class TestTaskManager:
 
     @pytest.fixture
     def setup(self):
+        self.task_manager = TaskManager('test_agent')
+        self.task_manager._dependency_manager = MagicMock()
+        self.task_manager.intrest_desc = MagicMock(spec=IntrestDescription)
         self.task_dict = {}
-        self.concrete_task_manager = MagicMock()
-        self.concrete_task_manager._task_dict = self.task_dict
-        self.concrete_task_manager._dependency_manager = MagicMock()
-        self.concrete_task_manager.intrest_desc = MagicMock(spec=IntrestDescription)
-        self.task_manager_interface = TaskManager(self.concrete_task_manager)
-
-    def test_init(self, setup):
-        assert self.task_manager_interface._task_dict == self.task_dict
-        assert self.task_manager_interface._TaskManagerInterface__concrete_task_manager == self.concrete_task_manager
-
-    def test_task_dict_property(self, setup):
-        assert self.task_manager_interface.task_dict == self.task_dict
+        self.task_manager._task_dict = self.task_dict
 
     def test_receive_task(self, setup):
         task_desc = '{"desc": "Test Task", "dependencies": []}'
         task_finished_callback = MagicMock()
-        self.task_manager_interface.receive_task('task_1', task_desc, task_finished_callback)
+        self.task_manager.receive_task('task_1', task_desc, task_finished_callback)
         
         assert 'task_1' in self.task_dict
         assert self.task_dict['task_1']._task_data.task_desc == json.loads(task_desc)
 
     def test_get_intrest(self, setup):
-        self.concrete_task_manager.intrest_desc = MagicMock(spec=IntrestDescription)
+        self.task_manager.intrest_desc = MagicMock(spec=IntrestDescription)
+        task_desc = '{"desc": "Test Task", "dependencies": []}'
+        task_finished_callback = MagicMock()
+        self.task_manager.receive_task('task_1', task_desc, task_finished_callback)
+        result = self.task_manager.get_intrest(1)
         
-        result = self.task_manager_interface.get_intrest(1)
-        
-        assert result == self.concrete_task_manager.intrest_desc
+        # TODO: this test should be corrected after replacing the fake intrest desc
+        # with proper implementation 
+        assert result == self.task_manager.intrest_desc
 
     def test_define_next_behavior(self, setup):
         # TODO: refactor this test
@@ -49,7 +45,7 @@ class TestTaskManagerInterface:
         
         self.task_dict[task_conv_msg.short_id] = task
         
-        result = self.task_manager_interface.define_next_behavior(task_conv_msg)
+        result = self.task_manager.define_next_behavior(task_conv_msg)
         
         assert result == 'response'
         task.get_next_message.assert_called_once_with(msg=task_conv_msg)
