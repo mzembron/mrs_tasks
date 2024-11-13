f91a78eeb50572b433968c9d4c90ef92a429d50b
mzembron <mateuszzembron@gmail.com>
2024-11-13 22:51:26 +0000

    feat: init knowledge base class

---
+++ mrs_main/mrs_main/knowledge_base/knowledge_base.py
@@ -0,0 +1,5 @@
+class KnowledgeBase:
+    """ Knowledge base - class responsible for managing information about the environment agent 
+        operates in, the tasks it is supposed to perform and its own abilities. """
+    def __init__(self):
+        pass
