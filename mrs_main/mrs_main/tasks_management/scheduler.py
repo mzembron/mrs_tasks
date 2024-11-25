906c9b061df7914a06a972452b3e74779ed7c0d1
mzembron <mateuszzembron@gmail.com>
2024-11-25 21:55:03 +0000

    feat: add resuming task execution method in scheduler

--- mrs_main/mrs_main/tasks_management/scheduler.py
+++ mrs_main/mrs_main/tasks_management/scheduler.py
@@ -11,7 +11,7 @@ class Scheduler:
         #      Additionally scheduler should allow only one task to be executed at the same time,
         #      other tasks should be planned or supervised (while other agents execute it)
         self._dependency_manager = dependency_manager
-        self.backlog = []
+        self.backlog = [] #queue of tasks scheduled for execution - possibly should be thread safe
 
     def append_task(self, task: Task):
         """ Appends a new task to the task queue """
@@ -20,4 +20,9 @@ class Scheduler:
     def get_next_task(self):
         """ Returns the next task to be executed """
         #TODO: implement the logic to pull the most appropriate task from the backlog
-        pass
+        self.backlog.pop(0) # task finished remove from scheduler backlog
+        # dummy implementation - get next one in FIFO manner
+        for task in self.backlog:
+            if self._dependency_manager.are_task_dependencies_met(task.short_id):
+                task.fsm.resume_after_finished_dependencies()
+                break # TODO: need to handle the case when no task can be executed
