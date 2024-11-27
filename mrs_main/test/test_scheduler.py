a3a70bc2836244af5a6756f515fce0efb10daf95
mzembron <mateuszzembron@gmail.com>
2024-11-27 21:53:03 +0000

    tests: fix failing test for get_next_task in scheduler

--- mrs_main/test/test_scheduler.py
+++ mrs_main/test/test_scheduler.py
@@ -15,23 +15,22 @@ def test_append_task(scheduler):
     scheduler.append_task(task)
     assert task in scheduler.backlog
 
-# def test_get_next_task(scheduler):
-#     task1 = MagicMock(spec=Task)
-#     task1.short_id = "task1"
-#     task2 = MagicMock(spec=Task)
-#     task2.short_id = "task2"
-#     task1.fsm = MagicMock()
-#     task2.fsm = MagicMock()
-#     task1.fsm.resume_after_finished_dependencies = MagicMock()
-#     task2.fsm.resume_after_finished_dependencies = MagicMock()
+def test_get_next_task(scheduler):
+    task1 = MagicMock(spec=Task)
+    task1.short_id = "task1"
+    task2 = MagicMock(spec=Task)
+    task2.short_id = "task2"
+    task1.fsm = MagicMock()
+    task2.fsm = MagicMock()
+    task1.fsm.resume_after_finished_dependencies = MagicMock()
+    task2.fsm.resume_after_finished_dependencies = MagicMock()
 
-#     scheduler.append_task(task1)
-#     scheduler.append_task(task2)
+    scheduler.append_task(task1)
+    scheduler.append_task(task2)
 
-#     scheduler._dependency_manager.are_task_dependencies_met = MagicMock(return_value=True)
+    scheduler._dependency_manager.are_task_dependencies_met = MagicMock(return_value=True)
 
+    scheduler.get_next_task()
 
-#     scheduler.get_next_task()
-
-#     assert task1.fsm.resume_after_finished_dependencies.called
-#     assert not task2.fsm.resume_after_finished_dependencies.called
+    assert not task1.fsm.resume_after_finished_dependencies.called # as it gets removed from the queue
+    assert task2.fsm.resume_after_finished_dependencies.called
