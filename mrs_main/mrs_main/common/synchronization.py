ece6f261515e3a0d670bbd643981a1d2f48d6c55
mzembron <mateuszzembron@gmail.com>
2024-12-27 23:39:10 +0000

    feat(WiP): add lock type checking in synchronized decorator

--- mrs_main/mrs_main/common/synchronization.py
+++ mrs_main/mrs_main/common/synchronization.py
@@ -1,5 +1,6 @@
 from functools import wraps
-from threading import RLock
+# from threading import RLock, _RLock
+import threading
 
 def synchronized(lock_attr_name: str):
     """ Synchronization decorator wrapping whole methods accessing specified attribute """
@@ -7,8 +8,9 @@ def synchronized(lock_attr_name: str):
         @wraps(f)
         def wrapped(self, *args, **kwargs):
             lock = getattr(self, lock_attr_name)
-            # if not isinstance(lock, RLock):
-            #     raise TypeError(f"The attribute '{lock_attr_name}' must be an instance of RLock")
+            dummy_rlock = threading.RLock()
+            if not isinstance(lock, type(dummy_rlock)):
+                raise TypeError(f"The attribute '{lock_attr_name}' must be an instance of RLock")
             with lock:
                 return f(self, *args, **kwargs)
         return wrapped
