0d0a1e71dc4cfd1fdfeedaf6bee6fe02de14c927
mzembron <mateuszzembron@gmail.com>
2025-03-07 00:37:59 +0100

    feat(WiP): addjust the mrs_main executor to start subprocess taking care of robot navigation

--- mrs_main/mrs_main/task_execution/concrete_executors/turtlebot_executor.py
+++ mrs_main/mrs_main/task_execution/concrete_executors/turtlebot_executor.py
@@ -1,15 +1,14 @@
 import threading
 import time
 from mrs_main.task_execution.concrete_executors.executor_interface import AbstractExecutor
-from std_msgs.msg import String
+import subprocess
 
 
 class TurtlebotExecutor(AbstractExecutor):
     """ class handles execution of tasks for specific type of agent,
     in this case it is a dummy executor, which does nothing """
-    def __init__(self, callback_on_finish, orders_manager, task_exec_length=5) -> None:
+    def __init__(self, callback_on_finish, orders_manager , task_exec_length=5) -> None:
         super().__init__(callback_on_finish)
-        # self.execution_thread = None
         self.task_exec_length = task_exec_length
         self.orders_manager = orders_manager
         self.execution_thread = threading.Thread(target=self.mock_task_execution)
@@ -28,11 +27,8 @@ class TurtlebotExecutor(AbstractExecutor):
         pass
 
     def mock_task_execution(self):
-        publisher = self.orders_manager.create_publisher(String, '/some_topic', 10)
-        msg = String()
-        msg.data = 'Hello, this is a message from the worker function!'
-        publisher.publish(msg)
-        time.sleep(self.task_exec_length)
+        self.start_new_script('task_execution/concrete_executors/tb3_send_goal.py')
+        # time.sleep(self.task_exec_length)
         self._on_execution_finished()
 
     def _on_execution_finished(self):
@@ -40,6 +36,15 @@ class TurtlebotExecutor(AbstractExecutor):
         print("[ DEBUG LOG ] !!!!!!!!!!!! Task execution finished !!!!!!!!!!!!")
         self.callback_on_finish()
 
+    def start_new_script(self, script_path):
+        try:
+            result = subprocess.run(['python3', script_path, 'tb1', str(3.0), str(0.0) ], capture_output=True, text=True)
+            if result.returncode == 0:
+                print("Script output:", result.stdout)
+            else:
+                print("Script error:", result.stderr)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     def __del__(self):
         """ Destructor to join the execution thread """
