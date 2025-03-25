d7ed12fb8c7245b77d8fc60d825b0d37f277cab5
mzembron <mateuszzembron@gmail.com>
2025-03-25 22:22:30 +0100

    feat(WiP): init system config

---
+++ mrs_main/mrs_main/common/mrs_config.py
@@ -0,0 +1,7 @@
+
+# Task section
+MAX_TASKS_SUPPORTED = 20
+TASKS_DECLARATION_REQ = 3 # in seconds, how often publish the tasks declaration/ task alignment msg
+
+# System section
+START_KICKING_THREAD = True # support for dependency management - adds additional thread for that purpose
