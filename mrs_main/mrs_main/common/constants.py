33f703da90627418f9a31749b81a949b95f1eadf
mzembron <mateuszzembron@gmail.com>
2025-05-01 18:19:38 +0200

    correct task definition topic name

--- mrs_main/mrs_main/common/constants.py
+++ mrs_main/mrs_main/common/constants.py
@@ -1,5 +1,4 @@
-TASKS_DEFINITION_TOPIC_NAME = 'mrs_tasks/task_definition'
-REDUNDANT_TASKS_DEFINITION_TOPIC_NAME = 'mrs_tasks/redundant_task_definition'
+TASKS_DEFINITION_TOPIC_NAME = '/mrs_tasks/task_definition'
 
 # TaskDesc dict keys
 TASK_DESC_DEPENDENCIES = 'dependencies'
